78b0aded7b511c1ca66513edfe16bb37ff5f4508
Felipe Dornelas <felipegabriel123dornelas@gmail.com>
2024-05-19 16:01:31 -0300

    Aula 19.05 aprendendo variaveis e operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
+import java.net.SocketTimeoutException;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Variáveis
+
+        String nomeTeste = "Felipe";
+        int idade = 19;
+        double peso = 58.0;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        //Date dataNascimento = new Date();
+
     }
 }
---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,21 @@
+
+public class operadores {
+     public static void main(String[] args) {
+          
+        // Aprendendo operadores
+        // tipos: Aritméticos, unários, relacionais, lógicos, ternários;
+
+          boolean condicao1 = false;
+          boolean condicao2 = true;
+
+          if (condicao1 && (7 > 4)) {
+               System.out.println("As duas condições são verdadeiras");
+          }
+
+          if (condicao1 || condicao2) {
+               System.out.println("Uma das condições são verdadeiras");
+          }
+
+          System.out.println("Fim");
+     }
+}
